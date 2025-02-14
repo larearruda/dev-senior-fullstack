d8d2f9a03b8921fa1005a4c62cd0bcc288fe712e
Larissa Arruda Leite da Silva <larissa.arrudaldasilva@kyndryl.com>
2025-02-14 15:34:20 -0300

    removendo a pasta de submodulo e criando endpoint de health da api

---
+++ back-api/src/health/health.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get, HttpCode } from '@nestjs/common';
+
+@Controller('health')
+export class HealthController {
+    
+    @Get()
+    @HttpCode(200)
+    health(): string {
+        return 'health ok';
+    }
+}
