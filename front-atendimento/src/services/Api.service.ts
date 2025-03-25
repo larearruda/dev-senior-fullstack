d6076a382890a61a4d9979fa8d0e9238170ebfb0
Lari Arruda <larissa.arruda94@gmail.com>
2025-03-25 11:30:31 -0300

    add variaveis de ambiente no .env

--- front-atendimento/src/services/Api.service.ts
+++ front-atendimento/src/services/Api.service.ts
@@ -1,14 +1,12 @@
 import axios from "axios";
 
-enum Urls {
-  BASE_URL_HTTPS = "https://fakestoreapi.com/",
-  BASE_URL_HTTP = "http://localhost:8080/",
-}
+const BASE_URL_HTTPS = process.env.API_URL_HTTPS;
+const BASE_URL_HTTP = process.env.API_URL_HTTP;
 
 export const ApiHttps = axios.create({
-  baseURL: Urls.BASE_URL_HTTPS,
+  baseURL: BASE_URL_HTTPS,
 });
 
 export const ApiHttp = axios.create({
-  baseURL: Urls.BASE_URL_HTTP,
+  baseURL: BASE_URL_HTTP,
 });
--- front-atendimento/src/services/Socket.service.ts
+++ front-atendimento/src/services/Socket.service.ts
@@ -1,6 +1,18 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://192.168.15.19:8080");
+const ENABLE_SOCKET_IO = process.env.ENABLE_SOCKET_IO === "true";
+const SOCKET_URL = process.env.SOCKET_URL;
+
+
+let socket: any;
+
+if (ENABLE_SOCKET_IO) {
+  socket = io(SOCKET_URL);
+  console.log("✅ Socket.IO habilitado.");
+} else {
+  console.warn("⚠️ Socket.IO está desabilitado por configuração.");
+}
+
 export interface ChatMessage {
   id: number;
   message: string;
@@ -9,27 +21,35 @@ export interface ChatMessage {
 }
 
 export const joinRoom = (room: string) => {
+  if (!ENABLE_SOCKET_IO) return;
+  
   console.log("central de atendimento - iniciando conexao em: ", room);
   socket.emit("joinRoom", room);
 };
 
 export const sendMessage = (room: string, message: string) => {
-  // console.log("frontend", { room, message });
-  var newMessageFromApp: ChatMessage = {
+  if (!ENABLE_SOCKET_IO) return;
+
+  const newMessageFromApp: ChatMessage = {
     id: Math.random(),
     datesent: new Date().toDateString(),
     origin: "BROWSER",
     message: message,
   };
+
   message = JSON.stringify(newMessageFromApp);
   console.log("Enviando mensagem do frontend", message);
   socket.emit("sendMessage", { room, message });
 };
 
 export const listenForMessages = (callback: (message: string) => void) => {
+  if (!ENABLE_SOCKET_IO) return;
+
   socket.on("receiveMessage", callback);
 };
 
 export const disconnectSocket = () => {
+  if (!ENABLE_SOCKET_IO) return;
+
   socket.disconnect();
 };
